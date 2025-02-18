aa20d45f40c5108ef22222025622c881c715e664
emil.beyerov <emil.beyerov@digigo.work>
2025-02-18 22:16:16 +0400

    Fixed: routing for footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +7,35 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentPageComponent: string | null = null;
   isDetailPage = false;
 
   constructor(private router: Router) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
+        this.setCurrentComponent(event.urlAfterRedirects);
         this.isDetailPage = event.url.includes('/blog-details') || event.url.includes('/service-detail');
       }
     });
+  }
+  //
+  //
+  // constructor(private router: Router) {
+  //   this.router.events.subscribe(event => {
+  //     if (event instanceof NavigationEnd) {
+  //       this.isDetailPage = event.url.includes('/blog-details') || event.url.includes('/service-detail');
+  //     }
+  //   });
+  // }
+
+  setCurrentComponent(url: string) {
+    const routeMap: { [key: string]: string } = {
+      '/about': 'about',
+      '/services': 'services',
+      '/contact': 'contact',
+      '/blogs': 'blogs'
+    };
+
+    this.currentPageComponent = routeMap[url] || null;
   }
 }
