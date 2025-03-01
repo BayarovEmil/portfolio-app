355fb3b4e655db55a19133109f645eecea901708
emil.beyerov <emil.beyerov@digigo.work>
2025-03-01 08:55:52 +0400

    Test-2

--- src/app/api-configuration.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import {environment} from "../environments/environment.prod";
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ApiConfiguration {
-  rootUrl: string = environment.apiUrl; // API URL-i environment faylından oxunur
-}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-app-backend-production.up.railway.app/api/v1'
+  apiUrl: 'https://portfolio-app-backend-production.up.railway.app'
 };
