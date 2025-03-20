ee769a733341c7d6e92a52cfd31b0b59b32d8e79
emil.beyerov <emil.beyerov@digigo.work>
2025-03-20 05:21:22 +0400

    fix:: services page

--- src/app/features/services/service/service.component.ts
+++ src/app/features/services/service/service.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { Router } from '@angular/router';
+import {JsonServiceService} from "../../../services/json-service.service";
+
 
 @Component({
   selector: 'app-services',
   templateUrl: './service.component.html',
   styleUrls: ['./service.component.scss']
 })
-export class ServiceComponent {
-  services = [
-    { id: 1, name: 'Müqavilələrin hazırlanması və hüquqi ekspertizası', image: 'assets/images/service1.jpg' },
-    { id: 2, name: 'Miqrasiya hüququ üzrə', image: 'assets/images/service2.jpg' },
-    { id: 3, name: 'Korporativ hüquq', image: 'assets/images/service3.jpg' },
-    { id: 4, name: 'Texnologiya hüququ üzrə', image: 'assets/images/service4.jpg' },
-    { id: 5, name: 'Əqli mülkiyyət hüququ üzrə', image: 'assets/images/service5.jpg' },
-    { id: 6, name: 'Əmək hüququ üzrə', image: 'assets/images/service6.jpg' },
-  ];
+export class ServiceComponent implements OnInit {
+  services: any[] = []; // JSON-dan gələn məlumatları saxlayacağıq
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private jsonServiceService: JsonServiceService
+  ) {}
+
+  ngOnInit() {
+    this.jsonServiceService.getServices().subscribe(data => {
+      this.services = data;
+    });
+  }
 
   navigateToDetail(service: any) {
     this.router.navigate(['/service-detail', service.id]).then(() => {
