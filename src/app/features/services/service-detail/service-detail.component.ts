70f7642d5f6cd4cd84f5ad85c1895386f8a8a993
emil.beyerov <emil.beyerov@digigo.work>
2025-03-20 05:35:03 +0400

    fix:: service-details page

--- src/app/features/services/service-detail/service-detail.component.ts
+++ src/app/features/services/service-detail/service-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import {JsonServiceService} from "../../../services/json-service.service";
 @Component({
   selector: 'app-service-detail',
   templateUrl: './service-detail.component.html',
@@ -8,24 +8,30 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ServiceDetailComponent implements OnInit {
   service: any;
-  services = [
-    { id: 1, name: 'Müqavilələrin hazırlanması və hüquqi ekspertizası', image: 'assets/images/service1.jpg', details: ['Detay 1', 'Detay 2', 'Detay 3'] },
-    { id: 2, name: 'Miqrasiya hüququ üzrə', image: 'assets/images/service2.jpg', details: ['Detay 1', 'Detay 2'] },
-    { id: 3, name: 'Korporativ hüquq', image: 'assets/images/service3.jpg', details: ['Detay 1', 'Detay 2', 'Detay 3'] },
-    { id: 4, name: 'Texnologiya hüququ üzrə', image: 'assets/images/service4.jpg', details: ['Detay 1', 'Detay 2'] },
-    { id: 5, name: 'Əqli mülkiyyət hüququ üzrə', image: 'assets/images/service5.jpg', details: ['Detay 1', 'Detay 2'] },
-    { id: 6, name: 'Əmək hüququ üzrə', image: 'assets/images/service6.jpg', details: ['Detay 1', 'Detay 2'] },
-  ];
+  services: any[] = [];
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private jsonService: JsonServiceService
+  ) {}
 
   ngOnInit() {
+    this.jsonService.getServices().subscribe(data => {
+      this.services = data;
+      this.loadService();
+    });
+  }
+
+  loadService() {
     this.route.paramMap.subscribe(params => {
       const serviceId = params.get('id');
-      if (serviceId) {
-        this.service = this.services.find(s => s.id.toString() === serviceId);
-      }
-    });
+      this.service = this.services.find(s => s.id.toString() === serviceId);
+
+    if (!this.service) {
+      this.router.navigate(['/services']);
+    }
+  })
   }
 
   navigateToDetail(service: any) {
